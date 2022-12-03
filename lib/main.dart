65a165ab4c57595cb81317f28b11b0354dd6bf07
김인범 <gim-inbeom@gim-inbeom-ui-MacBookPro.local>
2022-12-03 23:37:12 +0900

    FIX:resetting

--- lib/main.dart
+++ lib/main.dart
@@ -13,10 +13,8 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  // 웹 환경에서 카카오 로그인을 정상적으로 완료하려면 runApp() 호출 전 아래 메서드 호출 필요
-  WidgetsFlutterBinding.ensureInitialized();
 
-  // runApp() 호출 전 Flutter SDK 초기화
+  // runApp() 호출 전 Kakao SDK 초기화
   KakaoSdk.init(
     nativeAppKey: '${dotenv.env['NATIVE_APP_KEY']}',
     javaScriptAppKey: '${dotenv.env['JAVASCRIPT_APP_KEY']}',
